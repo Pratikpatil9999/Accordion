d4dfd66f39583b726f42f26b29c630b4d2a7c3f7
Pratik <prateekpatil9999@gmail.com>
2021-08-15 18:26:51 +0530

    added data.js file in src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Questions from './question'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="question">
+          <h1>Questions and Answers About login</h1> 
+        </div>
+       
+           <Questions/>
+      
     </div>
   );
 }
---
+++ src/question.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Questions = () => {
+    return (
+        <div>
+          <h1>what is yppu name? and address ahd</h1>  
+          <h1>what is yppu name? and address ahd</h1>  
+          <h1>what is yppu name? and address ahd</h1>  
+          <h1>what is yppu name? and address ahd</h1>  
+          <h1>what is yppu name? and address ahd</h1>  
+          <h1>what is yppu name? and address ahd</h1>  
+          <h1>what is yppu name? and address ahd</h1>  
+        </div>
+      );
+}
+ 
+export default Questions;
