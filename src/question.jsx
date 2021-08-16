161fca8e5ffe89ac11b95a2908770baf129e26ae
Pratik <prateekpatil9999@gmail.com>
2021-08-16 22:20:14 +0530

    added conditional rendering

--- src/question.jsx
+++ src/question.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React  from 'react';
 import { AiOutlineMinus,AiOutlinePlus } from "react-icons/ai";
+import { useState } from "react";
 const Question = ({info,title}) => {
+     const [showInfo,setShowInfo] =useState(false);
+     
     return (
-        <article>
-             <h2>{title}</h2>
+        <article className="question">
+             <header>
+               <h3>{title}</h3>
+               <button onClick={()=>setShowInfo(!showInfo)}>{!showInfo?<AiOutlinePlus/>:<AiOutlineMinus/>}</button>
+             </header>
+             {showInfo && <p>{info}</p>}
         </article>
       );
 }
